bb6a72e255bcb66ddb1755184ce3f87d2d93398b
Rohan rana <rohanrana33042@gmail.com>
2025-05-07 00:10:29 +0530

    Refactor database connection setup and add MongoDB dependency in package.json

--- Episode 5/database.js
+++ Episode 5/database.js
@@ -1,1 +1,31 @@
-const URI = "mongodb+srv://Rohanrana2003:Rohan@0274@node.qf4mfrd.mongodb.net/";
+const { MongoClient } = require("mongodb");
+
+const URI =
+  "mongodb+srv://Rohanrana2003:Rohan%400274@node.qf4mfrd.mongodb.net/";
+
+const client = new MongoClient(URI);
+const dbName = "HelloWorld";
+
+async function main() {
+  await client.connect();
+  console.log("Connected Successfully to the server");
+  const db = client.db(dbName);
+
+  const collection = db.collection("User");
+  // const data = { name: "Rana", age: 21 };
+
+  // const insertDocuments = await collection.insertOne(data);
+  // console.log("Inserted Documents:", insertDocuments);
+
+  // const findDocuments = await collection.find({}).toArray();
+  // console.log("Found Documents:", findDocuments);
+
+  const result = await collection.find({ age: "27" }).toArray();
+  console.log("Result", result);
+  return "Done";
+}
+
+main()
+  .then(console.log)
+  .catch(console.error)
+  .finally(() => client.close());
